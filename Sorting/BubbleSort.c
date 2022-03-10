1c7200f5f14d4f0b91e5f46ff91e707cc930f768
Mansi <mansistartup2024@gmail.com>
2022-03-10 16:34:03 +0530

    BubbleSort

---
+++ Sorting/BubbleSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Swap(int *x,int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+void BubbleSort(int A[],int n)
+{
+    int i,j,flag=0;
+    for(i=0;i<n-1;i++)
+    {
+        int flag=0;
+        for(j=0;j<n-i-1;j++)
+        {
+            if(A[j]>A[j+1])
+            {
+                Swap(&A[j],&A[j+1]);
+                flag=1;
+            }
+           
+        }
+         if(flag==0) 
+               break;
+    }
+}
+int main()
+{
+    int A[] = {4,8,3,10,7,2,12};
+    int n = 7;
+    int i;
+    BubbleSort(A,n);
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",A[i]);
+    }
+}
